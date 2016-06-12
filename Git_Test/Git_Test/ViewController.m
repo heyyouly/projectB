a0228f93a2c11314f446f5d61a42eded99faaeac
heyyouly <liuyang@liuyangdeMacBook-Pro.local>
2016-06-12 20:17:59 +0800

    添加点东西

---
+++ Git_Test/Git_Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Git_Test
+//
+//  Created by 刘洋 on 16/6/12.
+//  Copyright © 2016年 LY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Git_Test/Git_Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Git_Test
+//
+//  Created by 刘洋 on 16/6/12.
+//  Copyright © 2016年 LY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
